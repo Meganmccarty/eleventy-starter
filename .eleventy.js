4082410aa47f5d32f6d6c62384b1f3fd6b7f7885
Megan <megan_mccarty@hotmail.com>
2023-01-15 14:41:01 -0500

    Add JS and support for testing/linting

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
-    // Reload page when changes made to .scss files
+    // Reload page when changes made to .scss, .css, and .js files
     eleventyConfig.addWatchTarget('./src/scss/');
     eleventyConfig.addWatchTarget('./src/css/');
+    eleventyConfig.addWatchTarget('./src/js/');
     eleventyConfig.setBrowserSyncConfig({
         files: './dist/css/**/*.css'
     });
