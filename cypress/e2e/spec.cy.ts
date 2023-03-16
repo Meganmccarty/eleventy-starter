f797c9c82122a08d57284e578bf50484a01a12a9
Megan McCarty <megan@v-studios.com>
2023-03-16 17:57:50 -0400

    Improve terminal logging for cypress a11y violations

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,26 @@
 import 'cypress-axe';
 
+// Outputs specific a11y violations to terminal in tabular format
+function terminalLog(violations: Array<any>) {
+    cy.task(
+        'log',
+        `${violations.length} accessibility violation${
+            violations.length === 1 ? '' : 's'
+        } ${violations.length === 1 ? 'was' : 'were'} detected`,
+    );
+
+    const violationData = violations.map(({
+        id, impact, description, nodes,
+    }) => ({
+        id,
+        impact,
+        description,
+        nodes: nodes.length,
+    }));
+
+    cy.task('table', violationData);
+}
+
 describe('load page', () => {
     beforeEach(() => {
         cy.visit('http://localhost:8080/');
@@ -8,6 +29,6 @@ describe('load page', () => {
 
     it('has no detectable a11y violations on load', () => {
         // Test the page at initial load
-        cy.checkA11y();
+        cy.checkA11y(undefined, undefined, terminalLog);
     });
 });
